f1c7fc0ee3259eb14c20769facd8472dd9e8c58b
WLAngel <akloratok@gmail.com>
2017-06-29 11:24:36 +0800

    initial commit

---
+++ app.js
@@ -0,0 +1,58 @@
+var express = require('express')
+var path = require('path')
+var route = require('./src/route.js')
+var bodyparser = require('body-parser')
+
+var app = express()
+
+
+app.set('views', path.join(__dirname, 'views'))
+app.set('view engine', 'pug')
+
+app.use(express.static(path.join(__dirname, 'public')))
+app.use(bodyparser.json())
+app.use(bodyparser.urlencoded({ extended: true }))
+
+app.get('/:id', function(req, res) {
+  var id = req.params.id
+  route.getArticle({ _id: Number(id) }).then(item => {
+    res.render('article', item[0])
+  })
+})
+
+app.post('/create', function(req, res) {
+  route.create(req.body.title, req.body.author, req.body.body)
+  .then(x => {
+    res.redirect(302, '/'+x)
+  })
+})
+
+app.post('/:id/delete', function(req, res) {
+  var id = req.params.id
+  console.log(id)
+  route.remove({ _id: Number(id) })
+  res.redirect(302, '/')
+})
+
+app.get('/', function (req, res) {
+  var articles = []
+  route.getArticle({ delete: false }).then(item => {
+    for(var x = 0; x < item.length; x++) {
+      articles.push({
+        id: item[x]._id,
+        title: item[x].title,
+        author: item[x].author,
+        time: item[x].time
+      })
+    }
+    res.render('index', {
+      articles: articles
+    })
+  })
+})
+
+
+
+
+
+app.listen(8888)
---
+++ src/route.js
@@ -0,0 +1,66 @@
+var MongoClient = require('mongodb').MongoClient
+var assert = require('assert');
+
+
+
+module.exports = {
+  create: create,
+  getArticle: getArticle,
+  edit: edit,
+  remove: remove,
+}
+
+var url = 'mongodb://mongod:27017/microblog';
+
+
+
+function create(title, author, body) {
+  var id
+  return getArticle().then(item => item.length).then(x => {
+    id = x
+    MongoClient.connect(url, function(err, db) {
+      var collection = db.collection('blogs')
+      collection.insertOne({
+        _id: x,
+        title: title,
+        author: author,
+        body: body,
+        time: (new Date()).toString(),
+        delete: false
+      }, function(err, r) {
+        assert.equal(null, err)
+        db.close()
+      })
+    })
+    return id
+  })
+
+}
+
+function getArticle(query) {
+  var posts = []
+  return MongoClient.connect(url).then(function(db) {
+      var collection = db.collection('blogs')
+      if(query === undefined) {
+        return collection.find().toArray()
+      }
+      else {
+        return collection.find(query).toArray()
+      }
+    }).then(function(items) {
+      posts = items
+      return items
+    })
+}
+
+function edit() {
+
+}
+
+function remove(query) {
+  MongoClient.connect(url).then(function(db) {
+    console.log(query)
+    var collection = db.collection('blogs')
+    collection.updateOne(query, { delete: true })
+  })
+}
